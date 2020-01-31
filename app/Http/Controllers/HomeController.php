b5d8c7f7b81ced759eaca5f82205c32c7bf4c140
yigitoz <yigitoztemel@beyn.com.tr>
2020-01-31 18:04:56 +0300

    Test swoole

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends BaseController
         ++$num['value'];
         $this->table->set("num", $num);
 
-        $f = file_get_contents('../../../../test.txt');
+        $f = file_get_contents('/home/yigit/swoole-test/test.txt');
 
         return 'hello world!(' . $f . '):' . $num['value'];
     }

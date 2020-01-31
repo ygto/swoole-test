9d8a0ff21e65ef7c8c26702a480055510da71122
yigitoz <yigitoztemel@beyn.com.tr>
2020-01-31 15:25:28 +0300

    Test swoole

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 $http = new swoole_http_server("127.0.0.1", 9501);
 
+
+$table = new Swoole\Table(1024);
+$table->column('key', Swoole\Table::TYPE_STRING, 32);
+$table->column('value', Swoole\Table::TYPE_STRING, 1024 * 100);
+$table->column('expired_at', Swoole\Table::TYPE_INT);
+$table->create();
+
+
 $http->on("request", function ($request, $response) {
 
     $response->end('<html><body>Lucky number: ' . rand(1, 100) . '</body></html>');

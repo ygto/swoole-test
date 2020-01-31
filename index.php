fc874b60e33131e7dd30400b1877c3842f9ca8ca
yigitoz <yigitoztemel@beyn.com.tr>
2020-01-31 17:10:29 +0300

    Test swoole

--- index.php
+++ index.php
@@ -40,19 +40,34 @@ class Controller
 
         //var_dump($request);
         // Fetch method and URI from somewhere
-                $httpMethod = $request->server['request_method'];
-                $uri = $request->server['request_uri'];
+        $httpMethod = $request->server['request_method'];
+        $uri = $request->server['request_uri'];
 
         // Strip query string (?foo=bar) and decode URI
-                if (false !== $pos = strpos($uri, '?')) {
-                    $uri = substr($uri, 0, $pos);
-                }
-                $uri = rawurldecode($uri);
+        if (false !== $pos = strpos($uri, '?')) {
+            $uri = substr($uri, 0, $pos);
+        }
+        $uri = rawurldecode($uri);
 
-                $routeInfo = $this->dispatcher->dispatch($httpMethod, $uri);
+        $routeInfo = $this->dispatcher->dispatch($httpMethod, $uri);
+        var_dump($routeInfo);
+        /*switch ($routeInfo[0]) {
+            case FastRoute\Dispatcher::NOT_FOUND:
+                // ... 404 Not Found
+                break;
+            case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                // ... 405 Method Not Allowed
+                break;
+            case FastRoute\Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
 
-                var_dump($routeInfo);
 
+
+
+                break;
+        }*/
 
         $num = $this->table->get('num');
         ++$num['value'];

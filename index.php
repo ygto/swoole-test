23e5fca64a8b62654008319453ac3b693570312d
yigitoz <yigitoztemel@beyn.com.tr>
2020-01-31 15:18:33 +0300

    Test swoole

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$server = new Swoole\Server("127.0.0.1", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+$http = new swoole_http_server("127.0.0.1", 9501);
 
-$server->on("request", function($request, $response){
+$http->on("request", function ($request, $response) {
 
     $response->end('<html><body>Lucky number: ' . rand(1, 100) . '</body></html>');
 });
 
-$server->start();
+$http->start();
